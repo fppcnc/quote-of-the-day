1b595454e55679ab1a153a214cc1687814656c32
Fil <concatofilippo94@gmail.com>
2023-10-01 16:49:53 +0200

    Author written in black for visibility

--- src/QuoteDisplay.js
+++ src/QuoteDisplay.js
@@ -4,7 +4,7 @@ function QuoteDisplay({ quote }) {
     return (
         <div>
             <p className="text-2xl font-semibold mb-6">{quote.content}</p>
-            <p className="text-right text-xl text-gray-600 mb-6">- {quote.author}</p>
+            <p className="text-right text-xl text-black-600/75 mb-6">- {quote.author}</p>
         </div>
     );
 }
